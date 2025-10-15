fdf54ec73f3aed9e78b71adbc69e07f2ee842897
Sakey <sheikh36ahmed@gmail.com>
2025-10-15 19:11:49 +0100

    deployment setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        popup: 'index.html', 
+      },
+    },
   },
 });
